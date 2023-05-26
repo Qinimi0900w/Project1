ed70c085a1c89a662c34905798c2a9dae8d8350b
jasmin <merfigigi@gmail.com>
2023-05-27 00:19:29 +0600

    feat:changed

--- project2/src/components/header/header.jsx
+++ project2/src/components/header/header.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import "./header.css";
 import Logo from "./../img/logo.png";
-import Search from "./../img/search.png";
-import Profile from "./../img/Group.png";
-import Menu from "./../img/menu.png";
 import Slider from "./../img/sliders.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
 
 const ModalProfile = ({ active, setactive }) => {
     return (
         <div className={active ? "modal_profile_active" : "modal_profile"}>
-            <div className="modal_content" onClick={e => e.stopPropagation}>
+            <div className="modal_content" onClick={e => e.stopPropagation()}>
                 <FontAwesomeIcon icon={faXmark} className="btnexit" onClick={() => setactive(false)} />
                 <li>Мой профиль</li>
                 <hr style={{ color: 'ligthgrey', width: '140px' }} />
@@ -21,8 +22,19 @@ const ModalProfile = ({ active, setactive }) => {
         </div>
     )
 }
+const ModalBurger = ({ active, setactive }) => {
+    return (
+        <div className={active ? "modal_burger_active" : "modal_burger"}>
+            <div className="modal_content" onClick={e => e.stopPropagation()}>
+                <FontAwesomeIcon icon={faXmark} className="btnexit" onClick={() => setactive(false)} />
+                <li>Избранные новости</li>
+            </div>
+        </div>
+    )
+}
 const Header = () => {
-    const [modalActive, setModalActive] = useState(true);
+    const [modalActive, setModalActive] = useState();
+    const [modalActiveBur, setModalActiveBur] = useState(true);
     return (
         <>
             <header className="header_news">
@@ -33,28 +45,30 @@ const Header = () => {
                     <div className="link_pr">
                         <ul>
                             <li>
-                                <a href="">
-                                    <img src={Search} alt="" />
+                                <a href="/"  >
+                                    <FontAwesomeIcon icon={faMagnifyingGlass} className="navbtn" />
                                 </a>
                             </li>
                             <li>
-                                <a href="">
-                                    <img src={Profile} alt="" />
+                                <a href="/"  onClick={() => { setModalActive(true) }}>
+                                <FontAwesomeIcon icon={faUser} className="navbtn"/>
                                 </a>
                             </li>
                             <li>
-                                <a href="">
-                                    <img src={Menu} alt="" />
+                                <a href="/"  onClick={() => { setModalActiveBur(true) }}>
+                                    <FontAwesomeIcon icon={faBars} className="navbtn"/>
                                 </a>
                             </li>
                         </ul>
                         <ModalProfile active={modalActive} setactive={setModalActive} />
+                        <ModalBurger active={modalActiveBur} setactive={setModalActive} />
+
                     </div>
                 </div>
                 <p className="title">Новости</p>
             </header>
             <div className="slider_block">
-                <a href=""><img src={Slider} alt="" className="slider" style={{ display: 'none' }} /></a>
+                <a href=""><img src={Slider} alt="" className="slider" /></a>
             </div>
         </>
 
